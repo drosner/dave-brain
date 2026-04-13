91a69a0b262c80a8fbf69c86069e80c976fdfc97
drosner <davidmrosner@gmail.com>
2026-04-12 20:40:50 -0400

    logging

--- scripts/run-gmail-pull.sh
+++ scripts/run-gmail-pull.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
-set -a
-source /home/drosner/davebrain/.env
-set +a
-
-cd /home/drosner/davebrain
-
-deno run \
-  --allow-net \
-  --allow-read \
-  --allow-write \
-  --allow-env \
-  pull-gmail.ts --window=24h --labels=INBOX,SENT --limit=200
+cd "$(dirname "$0")"
+mkdir -p logs
+LOGFILE="logs/pull-gmail-$(date +%Y%m%d-%H%M%S).log"
+echo "Logging to: $LOGFILE"
+deno run --allow-net --allow-read --allow-write --allow-env --env-file=.env pull-gmail.ts "$@" 2>&1 | tee "$LOGFILE"      
